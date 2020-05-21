7d822aff3f7d43f5b1a4e9221b86ca3c5442dae5
goldenstar8080 <gdst8965@gmail.com>
2020-05-20 20:38:26 -0700

    Fixed issue on login with facebook app. Enabled current location check.

--- src/common/services/rn-firebase/auth/facebook.js
+++ src/common/services/rn-firebase/auth/facebook.js
@@ -1,46 +1,3 @@
-// import auth from '@react-native-firebase/auth';
-// import {AccessToken, LoginManager} from 'react-native-fbsdk';
-
-// const PROVIDER_ID = 'facebook.com';
-
-// async function loginWithFacebook() {
-//   try {
-//     // if (variant === 'UNLINK') {
-//     //   await user.unlink(PROVIDER_ID);
-//     // } else {
-//     const {isCancelled} = await LoginManager.logInWithPermissions([
-//       'email', 'public_profile',
-//     ]);
-
-//     if (isCancelled) {
-//       console.log('==== Facebook Auth Canceled');
-//     } else {
-//       const result = await AccessToken.getCurrentAccessToken();
-//       if (!result) {
-//         throw new Error(
-//           'No Access Token was returned from Facebook SDK.',
-//         );
-//       }
-
-//       const {accessToken} = result;
-
-//       const credential = auth.FacebookAuthProvider.credential(
-//         accessToken,
-//       );
-
-//       // if (variant === 'LINK') {
-//       //   await user.linkWithCredential(credential);
-//       // } else if (variant === 'SIGN_IN') {
-//         const res = await auth().signInWithCredential(credential);
-//       // }
-//     }
-//   } catch (error) {
-//     console.log('==== Facebook Auth Error', error.message);
-//   } finally {
-//     console.log('===== finally');
-//   }
-// }
-
 import {
   AccessToken,
   LoginManager,
@@ -48,11 +5,15 @@ import {
   GraphRequestManager
 } from 'react-native-fbsdk';
 import { firebase } from '@react-native-firebase/auth';
+import { Platform } from 'react-native';
 
 export const AUTH_PROVIDER = 'facebook.com';
 
 export async function loginWithFacebook() {
   try {
+    // Added login feature on only web browser
+    LoginManager.setLoginBehavior(Platform.OS === 'ios' ? 'browser' : 'WEB_ONLY');
+
     const result = await LoginManager.logInWithPermissions([
       'public_profile', 'email'
     ]);
